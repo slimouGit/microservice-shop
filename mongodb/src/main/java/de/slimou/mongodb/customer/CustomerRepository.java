865f2b25cfae882cb6bf4ecf19d470f9548e26a7
Salim Oussayfi <box@slimou.net>
2021-02-28 10:08:20 +0100

    MongoDB

---
+++ mongodb/src/main/java/de/slimou/mongodb/customer/CustomerRepository.java
@@ -0,0 +1,12 @@
+package de.slimou.mongodb.customer;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import java.util.List;
+
+public interface CustomerRepository extends MongoRepository<Customer, String> {
+
+    public Customer findByFirstName(String firstName);
+    public List<Customer> findByLastName(String lastName);
+
+}
